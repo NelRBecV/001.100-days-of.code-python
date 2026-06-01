516ad8f480d542bceb1f76dd56288c86be3f5ef7
NelRBecV <nelrbecv16@gmail.com>
2026-06-01 10:15:01 -0300

    Add files via upload

---
+++ day001/chalengeday01_02.py
@@ -0,0 +1,13 @@
+#Problem Proposed
+#Fix the code bellow
+
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is donne with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+#print(("New lines can be created with a bacslash and n.")
+#
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a bachslash an n.")
