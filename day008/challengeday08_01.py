eea21b4a2c01d9861ff87be508ae48f237456cea
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-06 23:00:43 -0300

    Create challengeday08_01.py

---
+++ day008/challengeday08_01.py
@@ -0,0 +1,14 @@
+def greet():#Simple Function without parameters
+    print("Hello everybody")
+    print("Having fun so far?")
+    print("I don't think so, jajajaja")
+
+greet()
+def greet(name):#Function with input parameters
+    print(f"Hello {name}")
+    print("Having fun so far?")
+    print(f"I don't think so, {name}. Jajajaja\n")
+
+greet("jose")
+
+greet("marco")
