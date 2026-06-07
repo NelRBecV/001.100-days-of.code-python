2fb56c75449f6e6d1b02cf3490b9ebafef0bfe30
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-06 23:18:42 -0300

    Create challengeday09_02.py

---
+++ day009/challengeday09_02.py
@@ -0,0 +1,20 @@
+travel_log = {"France": ["Paris", "Lille", "Nice", "Bourdeaux"],
+              "Germany": ["Berlin","Hamburg","Frankfurt","Dormunt"]
+              }
+travel_log = {"France": {"cities_visited":["Paris", "Lille", "Nice", "Bourdeaux"]},
+              "Germany": {"places_to_go":["Berlin","Hamburg","Frankfurt","Dormunt"],"money_spend": 12,
+                          "Stadiums_visited":["Eintrak Stadium","Borussia Park"]}
+            }
+# print(travel_log["Germany"])
+
+travel_log = [
+        { "country": "France",
+        "cities_visited":["Paris", "Lille", "Nice", "Bourdeaux"]},
+        
+        {"country": "Germany",
+         "places_to_go":["Berlin","Hamburg","Frankfurt","Dormunt"],
+         "money_spend": 12,
+         "Stadiums_visited":["Eintrak Stadium","Borussia Park"]}
+        ]
+
+print(travel_log)
