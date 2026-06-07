0456fb7a1e14ea957d4f597a0b661fb35a32c6ef
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-07 11:02:17 -0300

    Create challengeday13_01.py

---
+++ day013/challengeday13_01.py
@@ -0,0 +1,8 @@
+#DEBUGGING EXCERCISE
+number = int(input()) # Which number do you want to check?
+
+# A failure was found down here
+if number % 2 = 0: #An equal symbol is missing
+    print("This is an even number")
+else:
+    print("This is an odd number")
