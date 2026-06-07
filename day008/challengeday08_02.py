4cb0e8f7a503af6a661254b41e77fbe43a4f35e0
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-06 23:01:34 -0300

    Create challengeday08_02.py

---
+++ day008/challengeday08_02.py
@@ -0,0 +1,7 @@
+#Fuctions with more than 1 input
+def greet_with_loc(name, location):
+    print(f"Hello, {name}")
+    print(f"How are you doing in {location}?")
+    print(f"See you later, {name}")
+
+greet_with_loc(location="Arizona", name="Mike")
