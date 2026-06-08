e05a3251c71c15d5cc7531f0fe54966207583724
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-08 13:16:24 -0300

    Create food.py

---
+++ day020-021/food.py
@@ -0,0 +1,19 @@
+import random
+from turtle import Turtle
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+
+        self.shape('circle')
+        self.color('blue')
+        self.penup()
+        self.shapesize(0.5, 0.5)
+        self.speed('fastest')
+        self.refresh()
+
+    def refresh(self):
+        self.goto(random.randint(-280, 280, ), random.randint(-280, 280))
+      
