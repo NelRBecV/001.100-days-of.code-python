a417efa584d54542cc6b3a549719747232c0bfc2
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-07 22:09:47 -0300

    Create square.py

---
+++ day018/square.py
@@ -0,0 +1,34 @@
+import random  # Timmy the turtle
+from turtle import Turtle, Screen
+
+# import Turtle as t
+# tim = t
+timmy_the_turtle = Turtle()
+timmy_the_turtle.screen.bgcolor('white')
+timmy_the_turtle.shape('turtle')
+
+
+# As she did
+# for _ in range(10):
+    # timmy_the_turtle.left(45)
+    # timmy_the_turtle.forward(100)
+    # timmy_the_turtle.right(35)
+
+    # timmy_the_turtle.forward(7)
+    # timmy_the_turtle.penup()
+    # timmy_the_turtle.forward(7)
+    # timmy_the_turtle.pendown()
+    # timmy_the_turtle.left(90)
+
+
+# As I did
+timmy_the_turtle.forward(100)
+timmy_the_turtle.speed(1)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.home()
