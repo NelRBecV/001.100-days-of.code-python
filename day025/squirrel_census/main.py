56218136a8a909970321277d2e36bf421647458e
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-24 22:14:45 -0300

    Update main.py

--- day025/squirrel_census/main.py
+++ day025/squirrel_census/main.py
@@ -1,1 +1,12 @@
+import pandas
 
+squirrel = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+color_squirrel = squirrel["Primary Fur Color"]
+
+gray = color_squirrel[color_squirrel == "Gray"].count()
+red = color_squirrel[color_squirrel == "Cinnamon"].count()
+black = color_squirrel[color_squirrel == "Black"].count()
+
+squirrel_data = {"Fur Color": ["Gray", "Red", "Black"], "Count": [gray, red, black]}
+squirrel_count = pandas.DataFrame(squirrel_data).set_index("Fur Color")
+squirrel_count.to_csv("Squirrel_count.csv")
