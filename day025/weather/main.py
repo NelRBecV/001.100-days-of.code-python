d87070836d92c175a5c97f1f86275df0cb02c532
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-24 21:42:57 -0300

    Update main.py

--- day025/weather/main.py
+++ day025/weather/main.py
@@ -61,12 +61,3 @@ print(data[data.temp == data.temp.max()])
 # data_f = pandas.DataFrame(data_dict)
 # data_f.to_csv("csv_data.csv")
 # print(data_f)
-
-squirrel = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-color_squirrel = squirrel["Primary Fur Color"]
-gray = color_squirrel[color_squirrel == "Gray"].count()
-red = color_squirrel[color_squirrel == "Cinnamon"].count()
-black = color_squirrel[color_squirrel == "Black"].count()
-squirrel_data = {"Fur Color": ["Gray", "Red", "Black"], "Count": [gray, red, black]}
-squirrel_count = pandas.DataFrame(squirrel_data)
-squirrel_count.to_csv("Squirrel_count.csv")
