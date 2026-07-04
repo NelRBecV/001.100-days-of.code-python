70bfd658f17aef42a428fd122ac346a3058bf0cc
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-07-04 16:52:18 -0300

    Update mille_to_km_calculator.py
    add more functionallity to components in window

--- day027/mille_to_km_calculator.py
+++ day027/mille_to_km_calculator.py
@@ -1,20 +1,27 @@
 from tkinter import *
 
-def convert_m_km():
+
+def clear_entry(event=None):
+    e_num.delete(0, END)
+
+
+def convert_m_km(event=None):
     miles = e_num.get()
     km = round(float(miles) * 1.609, 2)
     l_quantity['text'] = str(km)
 
 
-FONT = ("Arial", 15 , "bold")
-window_converter= Tk()
+FONT = ("Arial", 15, "bold")
+window_converter = Tk()
 window_converter.title("Mile to Km Converter")
 window_converter.minsize(width=100, height=100)
 window_converter.config(padx=20, pady=30)
 
 e_num = Entry()
 e_num.config(width=8, font=FONT)
-e_num.insert(END,"0")
+e_num.insert(END, "0")
+e_num.bind("<Button-1>", clear_entry)
+e_num.bind("<Return>", convert_m_km)
 e_num.grid(column=1, row=0)
 
 l_miles = Label()
@@ -33,7 +40,7 @@ l_km = Label()
 l_km.config(text="Km", font=FONT)
 l_km.grid(column=2, row=1)
 
-b_calc= Button()
+b_calc = Button()
 b_calc.config(text="Calculate", command=convert_m_km, font=FONT)
 b_calc.grid(column=1, row=2)
 
