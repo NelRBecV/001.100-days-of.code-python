5b630d2f08d9b0f66ac38e724715126f533d8b6d
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-07-11 14:01:54 -0300

    Create main.py

---
+++ day032/motivational_quotes_sender/main.py
@@ -0,0 +1,24 @@
+import smtplib
+import datetime
+import dotenv
+import os
+from random import randint
+
+with open("data/quotes.txt", "r") as phrases:
+    quotes = phrases.readlines()
+
+quote_day = quotes[randint(0, len(quotes)-1)]
+todays_date = datetime.datetime.now()
+if todays_date.weekday() == 6:
+    with smtplib.SMTP("smtp.gmail.com", 587) as gmail_server:
+        user = os.getenv("USER_MAIL")
+        password = os.getenv("PSWD_MAIL")
+        dest = "ciber_espectro@yahoo.com.mx"
+        message = f"Subject: Today's Motivational Quote.\n\n {quote_day}"
+
+        gmail_server.starttls()
+        gmail_server.login(user=user, password=password)
+        gmail_server.sendmail(from_addr=user, to_addrs=dest, msg=message)
+        print("E-mail sent successfully")
+else:
+    print("Today is not Sunday")
