d8ab059ea81c7a4ea34e9bb63e3337530de6f676
Nelson Becerra <nelrbecv16@gmail.com>
2026-07-31 09:31:51 -0400

    separate hash symbol from text in line 1, add a blank line in line 5, separate hask symbol and text in line 6 and add some blank lines from lines 5 to 7, separate hash symbol to text in line 8, erase extra spacing in line 15 and line 18, move comment line to line 8

--- day005/Challengeday05_02.py
+++ day005/Challengeday05_02.py
@@ -1,18 +1,20 @@
-#input a lis of students scores
+# input a lis of students scores
 student_scores = input().split()
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
-#Don't change the code above
+
+# Don't change the code above
+
+# Write your code below this row
 high_score = 0
 low_score = 100
 for n in range(0, len(student_scores)):
     if high_score < student_scores[n]:
         high_score = student_scores[n]
     if low_score > student_scores[n]:
-         low_score = student_scores[n]
+        low_score = student_scores[n]
 
 lowest_score = str(low_score)
-if  low_score < 10:
+if low_score < 10:
     lowest_score = "0" + str(low_score)
 print(f"The Highest score in this class is: {high_score} and the lowest is: {lowest_score}")
-#Write your code below this row
