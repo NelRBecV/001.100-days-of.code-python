d9258598933058bcc823ecad5219fcf3b304c5d6
Nelson Becerra <nelrbecv16@gmail.com>
2026-07-31 10:03:11 -0400

    add blank spaces and change "to" word for "and" in line 1, add blank space in line 2, move comment line from line 3 to line 4, add a new line at the end of the file

--- day005/Challengeday05_03.py
+++ day005/Challengeday05_03.py
@@ -1,7 +1,8 @@
-target = int(input())#Enter a number between 0 to 1000
-#Do not change the code below
+target = int(input())  # Enter a number between 0 and 1000
+# Do not change the code below
+
+# Write your code below this row
 sum_even = 0
-#Write your code below this row
 for even in range(0, target+1, 2):
     sum_even += even
 
