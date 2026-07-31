a9f54469f96151d38de5685bdaabcc0f3f708c75
Nelson Becerra <nelrbecv16@gmail.com>
2026-07-31 09:25:04 -0400

    add a blank space in hash comment text in line 1, add a new space line in line 2, move "total_height" initialization from line 4 to line 9 and add its variable type, add a new loop in line 11 and rearrange code from line 7 to line 13, move "total_height" increment code to line 12

--- day005/Challengeday05_01.py
+++ day005/Challengeday05_01.py
@@ -1,12 +1,16 @@
-#input a Python list of students heights
+# input a Python list of students heights
+
 student_heights = input().split()
-total_height = 0
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
-#Don0t change the code above
-    total_height += student_heights[n]
 
-#Write your code below this row
+# Don't change the code above
+# Write your code below this row
+total_height: int = 0
+
+for h in range(len(student_heights)):
+    total_height += student_heights[h]
+
 print(f"Total Height: {total_height}")
 print(f"Number of Students: {len(student_heights)}")
 print(f"Average height: {round(total_height/len(student_heights))}")
