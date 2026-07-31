1b7966045316e5d1413fe7e4bebd32e458295397
Nelson Becerra <nelrbecv16@gmail.com>
2026-07-31 08:34:02 -0400

    add a blank space inside lines 1, 2 and 5 and add a new line at the end of file

--- day004/Challengeday04_02.py
+++ day004/Challengeday04_02.py
@@ -1,8 +1,8 @@
-#Write your code below this line
-#Hint: Don't forget make the call to the module random
+# Write your code below this line
+# Hint: Don't forget make the call to the module random
 import random
 
-coin_toss = random.randint(0,1)
+coin_toss = random.randint(0, 1)
 
 if coin_toss == 0:
     print("Tails")
