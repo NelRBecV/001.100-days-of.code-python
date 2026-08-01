f94bbba0ef02e9795fdba82369bc6f80b5b15e3c
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-01 11:06:27 -0400

    Rename file challenge_day_01_05.py

--- day001/challengeday01_05.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator")
-name_city=input("What is the name of the city where you grew up in?\n")
-name_pet=input("What is your pet's name?\n")
-print("Your band name could be " + name_city + " " + name_pet)
