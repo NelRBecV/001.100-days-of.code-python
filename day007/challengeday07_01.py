330491657837912a58e2a7aa649f4c8319f07f85
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-01 10:36:18 -0400

    Add a blank space in line 1, correct first word in "word_list", move TODO-1 description from line 10 to 5, add three variables called chosen_word, word_length, and display from lines 3 and 8. Move TODO-2 from line 9 to 15. Move code from line 38 to 18

--- day007/challengeday07_01.py
+++ day007/challengeday07_01.py
@@ -1,37 +1,26 @@
-#Step 1
+# Step 1
 import random
-word_list = ["ardvark", "baboon", "camel"]
 
-#TODO-1 Randomly choose a word from "word_list" and assign it
-#        to a variable called chosen_word
+word_list = ["aardvark", "baboon", "camel"]
+# TODO-1: Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+display = []
 
-# TODO-2 Ask the user to guess a letter and assing their variable called
-#        guess. Make gues lowcase
 
-# TODO-3 Check if the letter of the user guessed (guess) is one of the letters
-#         of the chosen word.
-life_left = 10
-chosen_word = word_list[random.randint(0,2)] #random.choice(word_list)
-chosen_word_letters = list(chosen_word)
-word_guessed = 0
-you_win=0
-you_lost=0
-while life_left > 0 or you_win != len(chosen_word):
-    guess=input("Choose a letter:\n") #guess=input("Choose a letter:").lower()
-    for i in range(0, len(chosen_word)):
-        if guess == chosen_word_letters[i] :
-            print("RIGHT!!")
-            word_guessed +=1
-            chosen_word_letters[i] = ""
-        else:
-            print("WRONG")
-            life_left -=1
+# For testing code purposes
+print(f'Pssst, the solution is {chosen_word}.')
 
-# if word_guessed == len(chosen_word):
-#     print("That's the correct word.")
-#     print("Congrats!! You WON!!!")
-# else:
-#     print("Sorry!!! The correct word was ")
-#     print("No lifes left. Game Over")
-#
 
+for _ in range(word_length):
+    display.append("_")
+
+# TODO-2: Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
+
+# TODO-3: Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for position in range(word_length):
+    letter = chosen_word[position]
+
+    if letter == guess:
+        print("YOU RIGHT!!!")
