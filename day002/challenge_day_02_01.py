64e43c9a7a0f0dfbef54fd550619011811aced53
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-01 11:13:39 -0400

    Rename file challenge_day_02_01.py

---
+++ day002/challenge_day_02_01.py
@@ -0,0 +1,6 @@
+two_digit_number = input()
+#Don't change the code above
+##################################################
+#Write your code below this line
+result = int(two_digit_number[0]) + int(two_digit_number[1])
+print(str(result))
