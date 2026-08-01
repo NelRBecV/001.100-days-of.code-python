f24c5dcda40355c6fcf1681a208b58de8b2d1763
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-01 11:21:03 -0400

    Rename file to challenge_day_05_03.py

--- day005/Challengeday05_03.py
+++
@@ -1,9 +0,0 @@
-target = int(input())  # Enter a number between 0 and 1000
-# Do not change the code below
-
-# Write your code below this row
-sum_even = 0
-for even in range(0, target+1, 2):
-    sum_even += even
-
-print(f"{sum_even}")
