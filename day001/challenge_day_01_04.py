adaaf8342f938306a81cc2ec229814cf9fa0de9f
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-01 11:06:14 -0400

    Rename file challenge_day_01_04.py

---
+++ day001/challenge_day_01_04.py
@@ -0,0 +1,13 @@
+#There are two varables, a and b from input
+a = input()
+b = input()
+
+#Don't try to change the code above
+############################################
+#Write your code below rhis line
+c = a
+a = b
+b = c
+#Write your code above this line
+print("a: " + a)
+print("b: " + b)
