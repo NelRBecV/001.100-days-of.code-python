01b1420c63621d764b0ac595c5a4cf05b5d6d40e
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-01 11:05:32 -0400

    Rename file challenge_day_01_01.py

--- day001/challengeday01_01.py
+++
@@ -1,6 +0,0 @@
-flour="500"; yeast="10"; water="300"; k_time="10"; salt="3"; k_rest="2"; b_temp="200"; b_time="30"
-print("1. Mix " + flour + "g of flour, " + yeast +"g of Yeast and " + water + "ml of water in a bowl.\n"
-      "2. Knead the dough for " + k_time + " min\n"
-      "3. Add " + salt + "g of salt\n"
-       "4. Leave to rise " + k_rest + " hours\n"
-       "5. Bake at " + b_temp + "degrees C for " + b_time + " min")
