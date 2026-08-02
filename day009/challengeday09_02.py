088af1075bb2ca973601c02b25b119d968ca0f50
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-02 10:30:38 -0400

    Add some comments to code, turn code into comment from lines 4 to 13, correct some misspelling words in lines 4, 5, 9, 10, 11, 19, 21, 23

--- day009/challengeday09_02.py
+++ day009/challengeday09_02.py
@@ -1,20 +1,26 @@
-travel_log = {"France": ["Paris", "Lille", "Nice", "Bourdeaux"],
-              "Germany": ["Berlin","Hamburg","Frankfurt","Dormunt"]
-              }
-travel_log = {"France": {"cities_visited":["Paris", "Lille", "Nice", "Bourdeaux"]},
-              "Germany": {"places_to_go":["Berlin","Hamburg","Frankfurt","Dormunt"],"money_spend": 12,
-                          "Stadiums_visited":["Eintrak Stadium","Borussia Park"]}
-            }
+# Nested dictionaries
+
+# Example - 1
+# travel_log = {"France": ["Paris", "Lille", "Nice", "Bordeaux"],
+#               "Germany": ["Berlin","Hamburg","Frankfurt","Dortmund"]
+#               }
+
+# Example - 2
+# travel_log = {"France": {"cities_visited": ["Paris", "Lille", "Nice", "Bordeaux"]},
+#               "Germany": {"places_to_go": ["Berlin", "Hamburg", "Frankfurt", "Dortmund"], "money_spend": 12,
+#                           "Stadiums_visited": ["Eintracht Stadium", "Borussia Park"]}
+#               }
+
 # print(travel_log["Germany"])
 
+# Example - 3
 travel_log = [
-        { "country": "France",
-        "cities_visited":["Paris", "Lille", "Nice", "Bourdeaux"]},
-        
+        {"country": "France",
+         "cities_visited": ["Paris", "Lille", "Nice", "Bordeaux"]},
         {"country": "Germany",
-         "places_to_go":["Berlin","Hamburg","Frankfurt","Dormunt"],
+         "places_to_go": ["Berlin", "Hamburg", "Frankfurt", "Dortmund"],
          "money_spend": 12,
-         "Stadiums_visited":["Eintrak Stadium","Borussia Park"]}
+         "Stadiums_visited":["Eintracht Stadium", "Borussia Park"]}
         ]
 
 print(travel_log)
