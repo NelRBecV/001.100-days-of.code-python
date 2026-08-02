1f640767e793ca337539ea2cbbb0bf6fbcb313e2
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-02 10:35:53 -0400

    Add some blank separators in lines 1, 2, 3, 25, 30, correct misspelling in line 19, change "visits" variable name to "number_of_visits"

--- day009/challengeday09_03.py
+++ day009/challengeday09_03.py
@@ -1,6 +1,6 @@
-country = input()#Add a country
-visits = int(input())#Number of visits
-list_of_cities = eval(input())#created list from formatted string
+country = input()  # Add a country
+visits = int(input())  # Number of visits
+list_of_cities = eval(input())  # created list from formatted string
 
 travel_log = [
     {
@@ -16,16 +16,18 @@ travel_log = [
 
 ]
 
-#Do no change the code above
+# Do not change the code above
 print(country)
 print(visits)
 print(list_of_cities)
 
-#Write the function that will allow new countries to be added to the travel log
-def add_new_country(nation,visits,cities):
-    travel_log.append({"country": country, "visits": visits, "list_of_cities": list_of_cities})
 
-#Don't change the code below
+# Write the function that will allow new countries to be added to the travel log
+def add_new_country(nation, number_of_visits, cities):
+    travel_log.append({"country": country, "visits": number_of_visits, "list_of_cities": list_of_cities})
+
+
+# Don't change the code below
 add_new_country(country, visits, list_of_cities)
 print(f"I've benn to {travel_log[2]['country']} {travel_log[2]['visits']}" + " times")
 print(f"My favourite city was {travel_log[2]['list_of_cities'][0]}")
