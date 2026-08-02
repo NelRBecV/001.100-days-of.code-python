c682a016702616cfec4457f922f9bb812143d301
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-02 12:01:05 -0400

    Add two new blank lines in rows 2, 3 and 19, remove extra space in row 5, reduce auction function parameters to one, change variable name in rows 37 and 38, move "auction_list" variable from auction function to inside "while" loop, take "auction" out of "while" loop,

--- day009/bidding_program.py
+++ day009/bidding_program.py
@@ -1,6 +1,8 @@
 from clear import clear
+
+
 def logo():
-    print ('''    
+    print('''    
                          ___________
                          \         /
                           )_______(
@@ -15,30 +17,27 @@ def logo():
                       /_______________\\
     ''')
 
-def auction(name, bid):
+
+def auction(auctions):
     offer_name = ""
     offer_bid = 0
-    auction_list.append({'Name': name, 'bid': bid})
-    name_big_bid=""
-    for i in range(len(auction_list)):
-        if offer_bid < auction_list[i]['bid']:
-            offer_name = auction_list[i]['Name']
-            offer_bid = auction_list[i]['bid']
-    if biders_left[0] == "n":
-        clear()
-        print(f"The biggest bid offered is ${offer_bid} by {offer_name}")
+    for i in range(len(auctions)):
+        if offer_bid < auctions[i]['bid']:
+            offer_name = auctions[i]['name']
+            offer_bid = auctions[i]['bid']
 
-    else:
-        clear()
+    print(f"The biggest bid offered is ${offer_bid} by {offer_name}")
 
 
 logo()
 print("Welcome to the secret auction program.")
 auction_list = []
-biders_left = "yes"
-while biders_left[0]=="y":
-    name = input("What's your name?: ")
-    bid = int(input("What is your bid?: $"))
-    biders_left = input("Are there any other biders?: Type 'yes' or 'no' ").lower()
-    auction(name,bid)
+bidders_left = "yes"
+while bidders_left[0] == "y":
+    bidder_name = input("What's your name?: ")
+    bid_amount = int(input("What is your bid?: $"))
+    auction_list.append({"name": bidder_name, "bid": bid_amount})
+    bidders_left = input("Are there any other bidders?: Type 'yes' or 'no' ").lower()
+    clear()
 
+auction(auction_list)
