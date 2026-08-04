e4c550fe25354d4b5c52233ffea14f0143efd4f3
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-04 10:52:36 -0400

    separate function definitions from "import" statement. Add ASCII main image to the program. Add extra line to separate functions in rows 27, 30, 34, 44, 45. Rename "opretations" variable name to "operations". Separate assignment symbol from "operations" and dict value. Align dict values in "operations" from lines 41 to 43. Separate "done_again" value from assignment symbol. Correct misspelling in row 55. Remove extra parentheses in row 56. Split "input" prompt message and separate assignment sign from values in rows 60 and 65. Separate equality sign in row 63. Add a new line in row 69

--- day010/challengeday10_03.py
+++ day010/challengeday10_03.py
@@ -1,45 +1,71 @@
 from clear import clear
+
+
 def logo():
-    logo=['''Again, here goes the logo, 
-        but, I don't have internet right now''']
+    app_logo = ["""
+ _____________________
+|  _________________  |
+| | JO           0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+
+    """]
+
 
 def add(num1, num2):
     return num1 + num2
 
+
 def subtract(num1, num2):
     return num1 - num2
+
 
 def multiply(num1, num2):
     return num1 * num2
 
+
 def divide(num1, num2):
     return num1 / num2
 
-opertations={'+': add,
-             '-': subtract,
-             '*': multiply,
-             '/': divide}
+
+operations = {'+': add,
+              '-': subtract,
+              '*': multiply,
+              '/': divide}
+
+
 def calculator():
     print(logo)
     num1 = float(input("What's the first number?: "))
-    done_again ="y"
+    done_again = "y"
   
     while done_again == "y":
-        for op in opertations:
+        for op in operations:
             print(op)
           
-        math_op = input("Which math opretation do yo want to do?:\n")
-        num2 = float(input(("What's the next number?: ")))
-        calculation_function = opertations[math_op]
-        resul= calculation_function(num1,num2)      
+        math_op = input("Which math operation do yo want to do?:\n")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[math_op]
+        resul = calculation_function(num1, num2)
         print(f"{num1} {math_op} {num2} = {resul}")
-        done_again=input(f"Type 'y' if you'd like to calculate with {resul}, or type 'n' if you want to calculate another number\n").lower()
+        done_again = input(f"Type 'y' if you'd like to calculate with {resul}, or type 'n' if you want to calculate"
+                           f" another number\n").lower()
       
-        if done_again=="y":
+        if done_again == "y":
             clear()
-            num1=resul
+            num1 = resul
         else:
             clear()
             calculator()
+
 
 calculator()
