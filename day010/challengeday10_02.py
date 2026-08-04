10e9995f8fbd35c5521529c20f1f18bb983c6de4
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-04 10:33:09 -0400

    Remove "leap" integer variable from code in row 3. Separate assignment symbol from rows 4 to 7. Remove prompt text in rows 8, 11, 13, and 16. Erase "leap" variable from rows 9, 14, and 17. Add "leap_year" variable in row 23. Rewrite "if" statement in line 24. Erase "days_month" and "g_year" from rows 24 and 25. Rename "days_month" variable to "days_of_month". Restructure "if" statement of leap year implementation. Assign a new return string value in row 32. Add some spaces in comment code in rows 37 and 38

--- day010/challengeday10_02.py
+++ day010/challengeday10_02.py
@@ -1,42 +1,40 @@
 def is_leap(year):
     """Verifies if a given year is a leap year. This is one of mines"""
-    leap=0
   
-    if year % 4 ==0:
-        if year % 100 ==0:
-            if  year % 400 == 0:
-                print("Leap year")
-                leap = 1
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
             else:
-                print("Not a leap year")
+                return False
         else:
-            print("Leap year")
-            leap = 1
+            return True
     else:
-        print("Not a leap year")
-    return leap
-  
-#TODO add more code here
+        return False
+
+
+# TODO add more code here
 def days_in_month(year, month):
     """shows the correspondent days for a chosen month. This is one of mines"""
-    month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
-    month_year = ['January','Febrary', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October','November','December']
-    days_month = ""
-    g_year = ""
-  
-    if month > 0 and month < 12:
-        if month == 2 and is_leap(year)==1:
-            days_month = month_year[month] + " has 29 days"
-            g_year = str(year) + " is a " + str(is_leap(year))
-        else:
-            days_month = month_year[month-1] + " has " + str(month_days[month-1]) + " days"
-            g_year = str(year) + " is not a " + str(is_leap(year))
-        return days_month; g_year
+
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month_year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                  'November', 'December']
+    leap_year = "not "
+    if 0 < month < 13:
+        days_of_month = month_days[month - 1]
+        if month == 2 and is_leap(year):
+            days_of_month += 1
+            leap_year = ""
     else:
         return "The month you introduced was not found"
-      
-#Do not change the code below
-year = int(input())#Enter a year
-month = int(input())#Enter a month
+
+    days_month = f"{month_year[month - 1]} has {days_of_month} days.\n{year} is {leap_year}a leap year."
+    return days_month
+
+
+# Do not change the code below
+year = int(input())  # Enter a year
+month = int(input())  # Enter a month
 days = days_in_month(year, month)
 print(days)
