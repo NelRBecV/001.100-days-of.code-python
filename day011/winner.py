a8e22a72ca5545cc392a525bd629b2a3e012cafa
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-07 21:46:31 -0400

    erase redundant winner.py file from project

--- day011/winner.py
+++
@@ -1,8 +0,0 @@
-def winner(win_player, win_computer):
-    if win_computer and win_player:
-        print("What do we got here? It's a PUSH!!!")
-    elif not win_computer and win_player:
-        print("Congratulations, pal. You WIN!!!")
-    else:
-        print("Sorry, man. You LOSE")
-
