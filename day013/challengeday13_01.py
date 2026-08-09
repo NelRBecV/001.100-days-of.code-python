0500627f9db3d74d0fff4f87e8cc601f9d1779a7
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-09 11:19:58 -0400

    correct misspelling in line 1, insert blank spaces inside comment line in line 5

--- day013/challengeday13_01.py
+++ day013/challengeday13_01.py
@@ -1,8 +1,8 @@
-#DEBUGGING EXCERCISE
+# DEBUGGING EXERCISE
 number = int(input()) # Which number do you want to check?
 
 # A failure was found down here
-if number % 2 = 0: #An equal symbol is missing
+if number % 2 = 0:  # An equal symbol is missing
     print("This is an even number")
 else:
     print("This is an odd number")
