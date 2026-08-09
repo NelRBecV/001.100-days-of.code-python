181d6181c81a961993805c2c5129a338fb58e5b2
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-09 11:21:34 -0400

    correct misspelling in lines 1, 5 insert blank spaces inside comment line in lines 2, 4 and 8

--- day013/challengeday13_02.py
+++ day013/challengeday13_02.py
@@ -1,11 +1,11 @@
-#DEBUGGING EXCERCISE
-#Which year do you want to check?
+# DEBUGGING EXERCISE
+# Which year do you want to check?
 
-#year = input()
-#Problem detectec: Non valid data input type. An integer data is required for processing data
+# year = input()
+# Problem detected: No valid data input type. An integer data is required for processing data
 #                  but the variable value gotten from it was string
 
-year = int(input()) #Casting input to integer solves the type incompatibility problem
+year = int(input())  # Casting input to integer solves the type incompatibility problem
 
 if year % 4 == 0:
     if year % 100 == 0:
