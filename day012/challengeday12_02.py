ca11e2542d84f57627155de3d5878145255bd74e
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-09 11:16:22 -0400

    insert blank line in row 3, remove extra space in ascii logo from lines 7 to 10, move "attempt_left" variable to line 15, modify "if" statement  and implementation in guessing_level method, reduce "finding_number" method parameters to one, erase "win" and "attempt" variables from "finding_number" method, reformat second if-statement in "finding_number" method, change return value from "finding_number" method,move "clear" and "logo" methods to lines 37 and 38, correct misspellings in lines 39, 42, 45 and 49; relocate "number_to_guess" and "lives_left" to line 39, rearrange code inside "while" loop

--- day012/challengeday12_02.py
+++ day012/challengeday12_02.py
@@ -1,59 +1,50 @@
 import random
 from clear import clear
 
+
 def logo():
     print('''
-            _  _   _  _   _   _  ___   ___   ____     ____   _  _   ___    ___    ___    ___   _  _   ____  
-            ) \/ ( ) () ( ) \_/ (\  _) ) __( /  _ \   ).-._( ) () ( ) __(  (  _(  (  _(  )_ _( ) \/ ( ).-._( 
-            |  \ | | \/ | |  _  ||  (  | _)  )  ' /   |( ,-. | \/ | | _)   _) \   _) \   _| |_ |  \ | |( ,-. 
-            )_()_( )____( )_( )_(/__o) )___( |_()_\   )_`__( )____( )___( )____) )____) )_____()_()_( )_`__( 
+    _  _   _  _   _   _  ___   ___   ____     ____   _  _   ___    ___    ___    ___   _  _   ____  
+    ) \/ ( ) () ( ) \_/ (\  _) ) __( /  _ \   ).-._( ) () ( ) __(  (  _(  (  _(  )_ _( ) \/ ( ).-._( 
+    |  \ | | \/ | |  _  ||  (  | _)  )  ' /   |( ,-. | \/ | | _)   _) \   _) \   _| |_ |  \ | |( ,-. 
+    )_()_( )____( )_( )_(/__o) )___( |_()_\   )_`__( )____( )___( )____) )____) )_____()_()_( )_`__( 
     ''')
 
-clear()
-logo()
 
 def guessing_level(level):
+    attempt_left = 5
     random_number = random.randint(1, 100)
-    if level[0] == "e":
+    if level.lower() == "easy":
         attempt_left = 10
-    else:
-        attempt_left = 5
 
     return random_number, attempt_left
 
 
-def finding_number(mistery_number, attempt):
+def finding_number(mistery_number):
     my_number = int(input("Make a guess: "))
-    win = 0
     if mistery_number == my_number:
         print("Congratulations!!! You did it")
-        win = 1
+        return 1
+
+    if mistery_number > my_number:
+        print("Too low")
     else:
-        if mistery_number > my_number:
-            print("Too low")
-            attempt -= 1
-        else:
-            print("Too high")
-            attempt -= 1
-        if attempt > 0:
-            print("Try again.")
-            print(f"You have {attempt} lives remaining")
-
-    return attempt, win
+        print("Too high")
+    return 0
 
 
+clear()
+logo()
 print("Welcome to the Number Guessing Game!!!")
+number_to_guess, lives_left = guessing_level(input("Choose a difficulty. Type 'easy' or 'hard': ").lower())
 print("I'm thinking of a number between 1 and 100.")
-game_level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-this_match = guessing_level(game_level)
-number_to_guess = this_match[0]
-guess_left = this_match[1]
-print(f"You have {guess_left} attempts to guess the number")
+while lives_left != 0:
+    print(f"You have {lives_left} attempts to guess the number")
+    result = finding_number(number_to_guess)
+    if result == 0:
+        lives_left -= 1
+    else:
+        break
 
-while guess_left != 0:
-        result = finding_number(number_to_guess, guess_left)
-        guess_left = result[0]
-        if guess_left == 0 and result[1] == 0:
-            print("No lifes left. Game Over!!!")
-        elif guess_left != 0 and result[1] != 0:
-            guess_left = 0
+    if lives_left == 0:
+        print("No lives left. Game Over!!!")
