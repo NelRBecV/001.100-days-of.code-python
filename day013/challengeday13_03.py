8c8ba7eb715bb37870faa9e71787146d8f440263
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-09 11:22:55 -0400

    correct misspelling in line 1, insert blank spaces inside lines in lines 5, 9, 12 and 16

--- day013/challengeday13_03.py
+++ day013/challengeday13_03.py
@@ -1,18 +1,17 @@
-#DEBUGGING EXCERCISE
+# DEBUGGING EXERCISE
 target = int(input())
 
 for number in range(1, target+1):
-    #The logical connector is "AND", not "OR"
+    # The logical connector is "AND", not "OR"
     if number % 3 == 0 or number % 5 == 0:
         print("FizzBuzz")
 
-    if number % 3 == 0: #This is an elif, not if
+    if number % 3 == 0:  # This is an elif, not if
         print("Fizz")
 
-    if number % 5 == 0: #This is an elif, not if
+    if number % 5 == 0:  # This is an elif, not if
         print("Buzz")
 
     else:
-        #There are additional bracket wrapping the variable
+        # There are additional bracket wrapping the variable
         print((number))
-      
