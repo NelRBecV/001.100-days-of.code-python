ee0348d57e65db78f31295f3d318d4fe9b5cd00d
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-09 10:18:11 -0400

    Separate hash symbol from text and correct some misspelling in rows 1 and 3, add a new line in rows 5 and 24, insert blank space in rows 4, 17 and 22, rename "bonification" misspelling method to "supplement"

--- day012/challengeday12_01.py
+++ day012/challengeday12_01.py
@@ -1,23 +1,27 @@
-#Introducing Local and GLobal Scopes and Namespaces
+# Introducing Local and Global Scopes and Namespaces
 
-#Example of an global Scope
-Money = 100 #This variable can be used all over the code
+# Example of a global Scope
+Money = 100  # This variable can be used all over the code
+
 
 # Example of a local variable
-def bonification():
+def supplement():
     Money = 200
 
-bonification()
-print(f"money with bonification {Money}")
-# It prints the value of global money, not the bonification module
 
-#Modifying a Global Variable
+supplement()
+print(f"money with extra payment {Money}")
+# It prints the value of global money, not the supplement module
+
+
+# Modifying a Global Variable
 def feeding_tickets():
     global Money
     Money += (Money * 1) 
                        
-#global keyword brings us values from inner scopes without returning anything but it must be used carefully
-#due to the high risk of getting bugs by changing the value of the variable itself
+# global keyword brings us values from inner scopes without returning anything, but it must be used carefully
+# due to the high risk of getting bugs by changing the value of the variable itself
+
 
 feeding_tickets()
 print(Money)
