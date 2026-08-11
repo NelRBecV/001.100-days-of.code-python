10c6d281510526f817100dc10539b2f014deda5a
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-10 21:25:49 -0400

    add a new line in row 2, remove extra space in lines 25 and 26

--- day014/hl_classical.py
+++ day014/hl_classical.py
@@ -5,10 +5,10 @@ def play_classical_mode(person1, person2):
     fans_person1 = person1['Fans']
     fans_person2 = person2['Fans']
     first_c()
-    
+
     print(f"This is {person1['Name']}. He/she is {person1['Occupation']} who is born in {person1['Country']}")
     print(f"and got {fans_person1} fans in his/her instagram account")
-    
+
     vs_logo()
     second_c()
     
@@ -16,13 +16,8 @@ def play_classical_mode(person1, person2):
     print(f"{person2['Name']}'s fans amount is higher or lower than {person1['Name']}")
     
     user_choice = input("Type 'h' for higher, or 'l' for lower: ").lower()
-    
-    print(fans_person1)
-    print(fans_person2)
-    
+
     if user_choice[0].lower() == "h":
         return fans_person2 > fans_person1
     elif user_choice[0].lower() == "l":
         return fans_person2 < fans_person1
-    
-
