8e0a0ae054101b3cd1fa49170b46844662fa9836
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-16 10:52:03 -0400

    add explanatory comments from line 5 to 10, from line 16 to line 21, from line 27 to line 32 and lastly from line 36 to line 39

--- day016/challengeday16_02.py
+++ day016/challengeday16_02.py
@@ -1,18 +1,41 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
+
+print("""
+First Table - unsorted
+=======================
+First column aligned to right
+Second column aligned to left    
+""")
 table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander'])
 table.add_column('Type', ['Electric', 'Water', 'Fire'])
 table.align = "r"
 print(table)
 
+print("""
+Second Table - sorted by 'Type'
+===============================
+First column aligned to center
+Second column aligned to left    
+""")
 table.align["Pokemon Name"] = "c"
 table.align['Type'] = "l"
 print(table)
 print(table.get_string(sortby='Type'))
 
+print("""
+Third Table - sorted by 'Pokemon Name'
+======================================
+First column aligned to right
+Second column aligned to left
+""")
 table.sortby = 'Pokemon Name'
 print(table)
 
+print("""
+Fourth Table - Add new rows
+===========================
+""")
 table.add_row(['Bulbazoor', 'Plant']); table.add_row(['Kadabra', 'Psiquic'])
 print(table)
