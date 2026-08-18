9dfa7030c5273c721414ad888f23101b33d62e49
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-18 12:23:52 -0400

    import segments module instead of turtle, add documentation comment in lines 5, 13, 18, 22, 31, 34, 36, 41, 46, 51 and 58; erase blank space in line 9, reformat "segments" function to "add_segments", reformat the "add_segment" function, add "get_location" function, reformat "moving" function to "move_forward", add return type in "move_up", "move_down", "move_left", "move_right", add "crash_wall" and "crash_ tail"

--- day020-021/snake.py
+++ day020-021/snake.py
@@ -1,26 +1,25 @@
-from turtle import Turtle
+from segments import Segment
 
 
 class Snake:
+    """Creates a snake, the playable character of the game."""
     def __init__(self):
         self.body = []
         self.count = 0
-
         for i in range(3):
-            self.segments()
+            self.add_segment()
 
-
-    def segments(self):
-        section = Turtle()
-        section.speed('fastest')
-        section.shape('square')
-        section.penup()
-        section.setx(20 * self.count)
-        section.color('white')
-        self.body.append(section)
+    def add_segment(self) -> None:
+        """Adds a new section to the snake."""
+        self.body.append(Segment(self.count))
         self.count += 1
 
-    def moving(self):
+    def get_location(self) -> tuple:
+        """Returns snake's head current position."""
+        return self.body[0].pos()
+
+    def move_forward(self) -> None:
+        """Moves the snake ahead."""
         for s in range(len(self.body) - 1, 0, -1):
             pos_x = self.body[s - 1].xcor()
             pos_y = self.body[s - 1].ycor()
@@ -28,19 +27,40 @@ class Snake:
 
         self.body[0].forward(10)
 
-    def move_up(self):
+    def move_up(self) -> None:
+        """Changes snake's orientation to up."""
         if self.body[0].heading() != 270:
             self.body[0].setheading(90)
 
-    def move_down(self):
+    def move_down(self) -> None:
+        """Changes snake's orientation to down."""
         if self.body[0].heading() != 90:
             self.body[0].setheading(270)
 
-    def move_left(self):
+    def move_left(self) -> None:
+        """Changes snake's orientation to the left."""
         if self.body[0].heading() != 0:
             self.body[0].setheading(180)
 
-    def move_right(self):
+    def move_right(self) -> None:
+        """Changes snake's orientation to the right."""
         if self.body[0].heading() != 180:
             self.body[0].setheading(0)
 
+    def crash_tail(self) -> bool:
+        """Checks if snake's head touches any section of its tail."""
+        for s_sec in range(3, len(self.body)):
+            if self.body[s_sec].distance(self.get_location()) <= 10:
+                return True
+        return False
+
+    def crash_wall(self, width: int, height: int) -> bool:
+        """Checks if snake's head touches any side of window border."""
+        width -= 30
+        height -= 20
+        # compensate unbalance measurement of inset
+        if -width - 10 > self.get_location()[0] or width < self.get_location()[0]:
+            return True
+        if -height + 10 > self.get_location()[1] or self.get_location()[1] > height:
+            return True
+        return False
