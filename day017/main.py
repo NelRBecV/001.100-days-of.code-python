790f120996cf1ae54b9e43b49de58be4dcb43a58
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-17 20:32:19 -0400

    shorten for-loop implementation

--- day017/main.py
+++ day017/main.py
@@ -3,14 +3,12 @@ from question_model import Question
 from data import question_data
 
 question_bank = []
-for i in range(len(question_data)):
-    question_game = Question(question_data[i]['question'], question_data[i]['correct_answer'])
-    question_bank.append(question_game)
+for item in question_data:
+    question_bank.append(Question(item['question'], item['correct_answer']))
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
-
 
 print("You completed the quiz")
 print(f"Your final score was: {quiz.score} right answers"
