59b37ed5f0028fc958a6a56c0f636d0a73aff965
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-19 22:18:49 -0400

    add documentation for "move_player", "reset_player", "set_player_health" and "is_crossed" methods

--- day023/player.py
+++ day023/player.py
@@ -14,15 +14,19 @@ class Player(Turtle):
         self.showturtle()
 
     def move_player(self):
+        """moves turtle up if it's still alive."""
         if self.__is_alive:
             self.forward(10)
 
     def reset_player(self):
+        """Returns player to the starting point."""
         self.goto(0, -280)
         self.setheading(90)
 
     def set_player_health(self, alive: bool):
+        """Changes turtle status."""
         self.__is_alive = alive
 
     def is_crossed(self):
+        """Checks if turtle successfully crossed the street."""
         return self.ycor() >= 280
