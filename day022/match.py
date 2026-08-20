c64bf4b8ddc6b9ec81f96827b6137357819fbceb
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-19 21:46:53 -0400

    correct misspelling in "turtle" module import, rename class "Field" to "Match", convert points individual variable into a list, change for-loop range, erase blank in line 10, refactor increment operation to "points" list, refactor "check_winner" method implementation, erase "player" parameter from "show_winner" method and add "check_winner" method inside "win.write" parameter, erase "net" method, reformat "scores" method

--- day022/match.py
+++ day022/match.py
@@ -1,15 +1,12 @@
-rom turtle import Turtle
+from turtle import Turtle
 
 
-class Field:
+class Match:
     def __init__(self):
-        self.points1 = 0
-        self.points2 = 0
+        self.points = [0]*2
         self.score = []
-        for i in range(3):
+        for i in range(2):
             self.score.append(Turtle())
-
-        for i in range(len(self.score) - 1):
             self.score[i].color('white')
             self.score[i].penup()
             self.score[i].speed('fastest')
@@ -18,53 +15,26 @@ class Field:
 
     def keeping_scores(self, side):
         if side <= -510:
-            self.points2 += 1
+            self.points[1] += 1
 
         elif side > 510:
-            self.points1 += 1
+            self.points[0] += 1
 
         self.scores()
 
     def check_winner(self):
-        if self.points1 == 15:
-            player = 1
-            self.show_winner(player)
-            return True
+        for p in range(len(self.points)):
+            if self.points[p] == 15:
+                return p+1
 
-        if self.points2 == 15:
-            player = 2
-            self.show_winner(player)
-            return True
-        return False
-
-    def show_winner(self, player):
-            win = Turtle()
-            win.hideturtle()
-            win.color('white')
-            win.penup()
-            win.write(f" Game Over\nPlayer {player} wins", False, "center", ("EightBit Atari", 40, "normal"))
+    def show_winner(self):
+        win = Turtle()
+        win.hideturtle()
+        win.color('white')
+        win.penup()
+        win.write(f" Game Over\nPlayer {self.check_winner()} wins", False, "center", ("EightBit Atari", 40, "normal"))
 
     def scores(self):
-        self.score[0].clear()
-        self.score[0].write(f"{self.points1}", False, "center", ('EightBit Atari', 30, 'normal'))
-        self.score[1].clear()
-        self.score[1].write(f"{self.points2}", False, "center", ('EightBit Atari', 30, 'normal'))
-
-    def net(self):
-        net_g = Turtle()
-        net_g.hideturtle()
-        net_g.color('white')
-        net_g.penup()
-        net_g.shape('square')
-        net_g.resizemode('user')
-        net_g.setpos(0, 500)
-        net_g.setheading(270)
-        net_g.shapesize(0.5, 1.0, 0.5)
-        net_g.pensize(4)
-
-        for i in range(40, -40, -1):
-            net_g.forward(10)
-            if i % 2 == 0:
-                net_g.pendown()
-            else:
-                net_g.penup()
+        for g_points in range(len(self.score)):
+            self.score[g_points].clear()
+            self.score[g_points].write(f"{self.points[g_points]}", False, "center", ('EightBit Atari', 30, 'normal'))
