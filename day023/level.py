eb73b33fb2a2bc31e73e55c898c10b007deac6d1
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-19 22:16:02 -0400

    add documentation for "enemies", "move_enemies", "scoreboard", "increment_points" and "is_endgame" methods, erase blank in line 44

--- day023/level.py
+++ day023/level.py
@@ -16,26 +16,30 @@ class Level:
         self.player_score = 1
 
     def enemies(self):
+        """creates enemies for the current level."""
         for i in range(20):
             self.enemies_level.append(Car())
 
     def move_enemies(self):
+        """Adds the ability to move for every single car in game."""
         for car in self.enemies_level:
             car.move_forward(self.player_score)
 
     def scoreboard(self):
+        """Shows scoreboard of the game."""
         self.scoreb.clear()
         self.scoreb.write(f"Level: {self.player_score}", False, "left",
                           ("EightBit Atari", 10, "normal"))
 
     def increment_scoreboard(self):
+        """Adds up to one point each time turtle steps forward."""
         self.player_score += 1
 
     def is_endgame(self, player: Turtle):
+        """Checks if the turtle was hit bya any car."""
         for car in self.enemies_level:
             if car.hit_turtle(player):
                 self.g_over.write("GAME OVER!", False, "center",
                                   ("Arial", 30, "bold"))
                 return True
-
         return False
