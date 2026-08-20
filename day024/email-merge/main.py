a666bdc29e5d34c1641ec3e57cc93546ebe7fe76
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-19 22:24:04 -0400

    erase blank in line 5 and 6, separate comment line from code in lines 4, 7 and 10, erase unnecessary "name_list" variable copy in line 4, separate "open" method parameters in line 15

--- day024/email-merge/main.py
+++ day024/email-merge/main.py
@@ -1,20 +1,18 @@
 import os
 
 with open("/input/names/invited_names.txt") as names:
-    name_list = names.readlines()# Reads every single line in a txt file
-    name_list
+    name_list = names.readlines()  # Reads every single line in a txt file
     names.close()
-
 with open("/input/letters/starting_letter.txt") as letter:
-    letter_content = letter.read()# Read all the content in a txt file
+    letter_content = letter.read()  # Read all the content in a txt file
     letter.close()
-    
-if not os.path.exists("ReadyToSend"):# Checks if the directory exists
+
+if not os.path.exists("ReadyToSend"):  # Checks if the directory exists
     os.mkdir("./ReadyToSend")
 
 for inv_name in name_list:
     invitation = letter_content.replace("[name]", f"{inv_name[0:len(inv_name)-1]}")
-    guest = open(f"./ReadyToSend/letter_for_{inv_name[0:len(inv_name)-1]}.txt","w")
+    guest = open(f"./ReadyToSend/letter_for_{inv_name[0:len(inv_name)-1]}.txt", "w")
     
     guest.write(invitation)
     guest.close()
