902a648c738ebcb6e19c221bbc07d634b91c1b8e
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-23 09:04:45 -0400

    change import line in lines 1 and 2, rename "snake" method to "create_snake_head", rename "segments" method to "add_segment", rename "new_game" method to "reset_snake_body" and change implementation in it, move snake body invocation method to the end of "reset_snake_body", reformat "crash_wall" and "crash_tail" implementation, create "is_crashed" method, add return type to methods in lines 58, 62 and 68

--- day024/enhanced snake game/snake.py
+++ day024/enhanced snake game/snake.py
@@ -1,69 +1,77 @@
-from turtle import Turtle
+from segment import Segment
+from turtle import Screen
 
 
 class Snake:
     def __init__(self):
         self.body = []
         self.count = 0
-        self.snake()
+        self.create_snake_head()
 
-    def snake(self):
+    def create_snake_head(self):
+        """Initializes the playable character of the game (the snake)."""
         for i in range(3):
-            self.segments()
-        return self.body
+            self.add_segment()
 
-    def segments(self):
-        section = Turtle()
-        section.speed('fastest')
-        section.shape('square')
-        section.penup()
-        section.setx(20 * self.count)
-        section.color('white')
-        self.body.append(section)
+    def add_segment(self):
+        """Increments the body of the snake by one piece."""
+        self.body.append(Segment(self.count))
         self.count += 1
 
-    def new_game(self):
-        for bs in range(len(self.body)):
-            self.body[bs].goto(800, 800)
-
-        self.body.clear()
+    def reset_snake_body(self, screen: Screen):
+        """Returns body of the snake to its initial state."""
         self.count = 0
-        self.snake()
+        for section in self.body:
+            section.color('black')
+            screen._turtles.remove(section)
+        self.body.clear()
+        self.create_snake_head()
 
     def moving(self):
-        self.body[0].forward(10)
-      
+        """Gets snake constantly moving through the game."""
         for s in range(len(self.body) - 1, 0, -1):
             pos_x = self.body[s - 1].xcor()
             pos_y = self.body[s - 1].ycor()
             self.body[s].goto(pos_x, pos_y)
+        self.body[0].forward(10)
 
     def move_up(self):
+        """Set snake's direction to UP."""
         if self.body[0].heading() != 270:
             self.body[0].setheading(90)
 
     def move_down(self):
+        """Set snake's direction to DOWN."""
         if self.body[0].heading() != 90:
             self.body[0].setheading(270)
 
     def move_left(self):
+        """Set snake's direction to LEFT."""
         if self.body[0].heading() != 0:
             self.body[0].setheading(180)
 
     def move_right(self):
+        """Set snake's direction to RIGHT."""
         if self.body[0].heading() != 180:
             self.body[0].setheading(0)
 
-    def get_head_pos(self):
+    def get_head_pos(self) -> tuple:
+        """Returns a tuple with x-y coordinates of snake."""
         return self.body[0].pos()
 
-    def crash_wall(self):
-        head = self.body[0]
-        if not -280 <= head.xcor() <= 270 or not -270 <= head.ycor() <= 280:
+    def crash_wall(self) -> bool:
+        """Checks if snake's head hits any of the walls (window borders) of the game."""
+        x, y = self.get_head_pos()
+        if not -280 <= x <= 270 or not -270 <= y <= 280:
             return True
 
-    def crash_tail(self):
-        head_x, head_y = self.body[0].pos()
+    def crash_tail(self) -> bool:
+        """Checks if the snake's head touches one section of its tail."""
+        head_x, head_y = self.get_head_pos()
         for sec in range(3, len(self.body)):
             if self.body[sec].distance(head_x, head_y) <= 10:
                 return True
+
+    def is_crashed(self):
+        """Returns 'True' if snake's head touches the walls or its own tail. Otherwise, returns 'False'."""
+        return self.crash_wall() or self.crash_tail()
