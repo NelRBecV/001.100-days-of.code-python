a010284cdb3a1d076585fc3a7ce2130c1ac0f86c
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-23 10:45:41 -0400

    erase "nato" list variable initialization in line 9, arrange "nato_alphabet" dictionary values from lines 1 to 8, correct misspelling in line 10, add blank line in line 15, separate hash comment in line 16

--- day026/nato_alphabet_conversor.py
+++ day026/nato_alphabet_conversor.py
@@ -1,19 +1,17 @@
 nato_alphabet: dict = {"A": "Alpha", "B": "Bravo", "C": "Charlie", "D": "Delta",
-                 "E": "Echo", "F": "Foxtrot", "G": "Golf", "H": "Hotel",
-                 "I": "India", "J": "Juliett", "K": "Kilo", "L": "Lima",
-                 "M": "Mike", "N": "November", "O": "Oscar", "P": "Papa",
-                 "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango",
-                 "U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "Xray",
-                 "Y": "Yankiee", "Z": "Zulu"
-                 }
-nato: list = []
+                       "E": "Echo", "F": "Foxtrot", "G": "Golf", "H": "Hotel",
+                       "I": "India", "J": "Julliet", "K": "Kilo", "L": "Lima",
+                       "M": "Mike", "N": "November", "O": "Oscar", "P": "Papa",
+                       "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango",
+                       "U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "Xray",
+                       "Y": "Yankiee", "Z": "Zulu"}
 
 # Get word letters
-word = list(input("Python's Nato Alphabeth Conversor\nEnter a word: ").upper()) #Method 1
-# name.split() #Method 2
+word = list(input("Python's Nato Alphabet Conversor\nEnter a word: ").upper())  # Method 1
 
 # Get NATO code from given letters
 # for letter in name: #method 1
 #     nato.append(nato_alphabet[letter.upper()])
-nato: list = [nato_alphabet[letter] for letter in word if letter in nato_alphabet] #Method 2
+
+nato: list = [nato_alphabet[letter] for letter in word if letter in nato_alphabet]  # Method 2
 print(nato)
