05c1ed9154cb109f01c9c9d11d1006de0fb974d9
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-23 12:37:16 -0400

    move "add" function invocation from line 13 to 8, enable code to input user custom numbers

--- day027/arguments.py
+++ day027/arguments.py
@@ -5,9 +5,7 @@ def add(*num):
     print(result)
 
 
-# num = input("Introduce a lot of numbers: ").split()
-# alo_numbers = [int(q) for q in num]
-# add(alo_numbers)
-# result = [add(cipher) for cipher in alo_numbers]
-# print(result)
 add(15, 25, 80, 45, 30, 90)
+numbers = input("Introduce a lot of numbers: ").split(" ")
+alo_numbers = [int(q) for q in numbers]
+add(*alo_numbers)
