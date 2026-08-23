b17183276c88b9aabd5f05f40f6c25f35b8c1d50
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-23 09:40:26 -0400

    reduce import statement to one line, erase class heritage parameter from Scoreboard class, erase Turtle variable reference variables from constructor, create two ScreenText reference variable, reformat "show_score_game" implementation, erase "crash" method from class and move "show_highscore" to its place, rename "reset_game" to "reset_scoreboard" and move "increment_points" to line 21, reformat file opening command, reformat "game_over" method implementation, erase blank in lines in 9, 28, 38, 39 and 42

--- day024/enhanced snake game/scoreboard.py
+++ day024/enhanced snake game/scoreboard.py
@@ -1,35 +1,30 @@
-from turtle import Turtle
-from snake import Snake
+from screen_text import ScreenText
 
 
-class Scoreboard(Turtle):
+class Scoreboard:
     def __init__(self):
         super().__init__()
         self.hs = 0
         self.score = 0
-        self.high_s = Turtle()
-        self.high_s.penup()
-        self.high_s.goto(220, 280)
-        self.high_s.color('white')
-        self.high_s.hideturtle()
         self.file_path = "record.txt"
+        self.current_score = ScreenText()
+        self.high_scores = ScreenText()
 
     def show_score_game(self):
-        self.hideturtle()
-        self.penup()
-        self.color('white')
-        self.goto(0, 280)
-        self.write(f"Score: {self.score}", False, "center", ("Courier", 12, "bold"))
+        self.current_score.set_pos((0, 280))
+        self.current_score.set_text(f"Score: {self.score}")
 
-    def crash(self, snake: Snake):
-        if snake.crash_wall() or snake.crash_tail():
-            return True
+    def show_highscore(self):
+        self.high_scores.set_pos((220, 280))
+        self.high_scores.set_text(f"Hi-score: {self.hs}")
 
-    def reset_game(self):
+    def increasing_points(self):
+        self.score += 1
+
+    def reset_scoreboard(self):
         try:
-            record = open(self.file_path, "r")
-            max_point: str = record.read()
-            record.close()
+            with open(self.file_path, "r") as record:
+                max_point: str = record.read()
         except FileNotFoundError:
             max_point: str = "0"
 
@@ -40,25 +35,9 @@ class Scoreboard(Turtle):
             self.hs = self.score
             with open(self.file_path, "w") as record:
                 record.write(str(self.score))
-
         self.score = 0
-
         self.show_score_game()
         self.show_highscore()
 
-    def show_highscore(self):
-        self.high_s.write(f"Hi-score: {self.hs}", False, "Center", ("Courier", 12, "bold"))
-
-    def increasing_points(self):
-        self.score += 1
-        self.show_score_game()
-
     def game_over(self):
-        loss = Turtle()
-        loss.hideturtle()
-        loss.color('white')
-        loss.write("You lost!", False, "center", ("Courier", 24, "bold"))
-
-    def clear_scores(self):
-        self.high_s.clear()
-        self.clear()
+        ScreenText(text="You lost!", move=False, align="center", font=("Courier", 24, "bold"))
