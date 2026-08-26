ba3d1c7ed571661898b996942765794983fb4107
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-26 10:25:55 -0400

    Add blank spaces in line 5, 6, 9, 16 and 34. Add new line in 6 and 25. Add new line in 8, 10 and 11. Correct misspelling in line 19

--- day030/nato_alphabet_v2.py
+++ day030/nato_alphabet_v2.py
@@ -2,18 +2,21 @@ import pandas
 
 
 nato_res = pandas.read_csv("nato_alphabet.csv")
-#nato_alphabet = {row.letter: row.code for (index, row) in nato_res.iterrows()}
-#iterrows, as its name says, iterate over DataFrame rows as (index, Series) pairs.
+# nato_alphabet = {row.letter: row.code for (index, row) in nato_res.iterrows()}
+# iterrows, as its name says, iterate over DataFrame rows as (index, Series) pairs.
 
-nato_alphabet = {nato_res["Letter"][letter]:nato_res["Word"][letter] for letter in range(len(nato_res["Letter"]))}
+# method 1
+nato_alphabet = {nato_res["Letter"][letter]: nato_res["Word"][letter] for letter in range(len(nato_res["Letter"]))}
+
+# method 2
 # nato_letter = nato_alphabet.keys()
 # nato_word = nato_alphabet.values()
 # nato_a = pandas.DataFrame({"Letter": nato_letter, "Word": nato_word})
 # nato_a.to_csv("nato_alphabet.csv", index=False)
-#index=False prevents pandas from adding the goddamn "Zero" column
+# index=False prevents pandas from adding the goddamn "Zero" column
 
 nato: list = []
-print("Python's Nato Alphabeth Conversor")
+print("Python's Nato Alphabet Conversor")
 
 # my method
 while True:
@@ -28,7 +31,7 @@ while True:
         print(f"Your NATO word is: {nato}")
         break
 
-#Angela's Method
+# Angela's Method
 # def generate_phonetics():
 #     word = list(input("Enter a word: ").upper())
 #     try:
