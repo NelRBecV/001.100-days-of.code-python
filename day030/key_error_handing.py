2829f27213e752e4fee0501c9c974aa02cf63718
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-26 09:50:41 -0400

    Add blank spaces in line 1, 11 and 19. Add new line in 6 and 25. correct misspelling and rearrange comment values in line 5 and 6

--- day030/key_error_handing.py
+++ day030/key_error_handing.py
@@ -1,13 +1,14 @@
-#eval() function create a list of dictionary using that looks like this (see data in line 5)
+# eval() function create a list of dictionary using that looks like this (see data in line 5)
 facebook_posts = eval(input())
 
 # data introduced to build the list
-# {"Likes": 21, "Coments":2}, {"Likes": 13, "Coments":2, "Shares": 2}, {"Likes": 33, "Coments":8, "Shares": 2}, {"Coments":2, "Shares": 2}, {"Coments":2, "Shares": 1}, {"Likes": 19, "Coments":3}
+# {"Likes": 21, "comments":2}, {"Likes": 13, "comments":2, "Shares": 2}, {"Likes": 33, "comments":8, "Shares": 2},
+# {"comments":2, "Shares": 2}, {"comments":2, "Shares": 1}, {"Likes": 19, "comments":3}
 
 total_likes: int = 0
 likes: int = 0
 
-#TODO: Catch th error exception
+# TODO: Catch th error exception
 print(type(facebook_posts))
 print(facebook_posts)
 
@@ -15,7 +16,7 @@ for post in facebook_posts:
     try:
         likes = post['Likes']
     except KeyError:
-        total_likes +=0
+        total_likes += 0
     else:
         total_likes = total_likes + likes
 
