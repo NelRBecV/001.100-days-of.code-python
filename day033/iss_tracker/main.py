8e0edd6421adad463c8678a1cb7d9b2041448471
Nelson Becerra <nelrbecv16@gmail.com>
2026-08-29 09:59:10 -0400

    Add "time" module to project. Remove "tkinter" and "dotenv" modules from project. Disable locally environment variables import. Protect target coordinates in lines 12 and 13. Remove tkinter "t" object from code. Create and implement"get_iss_location" function. Create and implement "get_local_daylight" function. Add "iss_locator" implementation inside while-loop. Add blank spaces in line 41. Change last comparison variable "iss_latitude" to "iss_longitude". Reformat "parameters" values. Erase code to be substituted with "get_local_daylight" function. Refactor "my_hour" getting value and check daylight if-statement. Protect "serv" and "port" values by turning them into env's and implementing them in server opening connection. Erase else-statement from main if-statement. Replace tkinter "after" method for timer "sleep". Reformat "main" if-statement. Delete "iss_locator" parameter from function. Erase tkinter "mainloop" from code. Reformat comment block from lines 69 to 72

--- day033/iss_tracker/main.py
+++ day033/iss_tracker/main.py
@@ -1,64 +1,72 @@
+import time
 import requests
 from datetime import datetime
 import smtplib
-from tkinter import *
 import os
-from dotenv import load_dotenv()
 
-load_dotenv()
+# To locally run this script, you must uncomment this lines bellow and add your own environment variables
+# from dotenv import load_dotenv
+#
+# load_dotenv()
 
-MY_LAT = 7.8160153271683965
-MY_LONG = -72.26874225465156
+MY_LAT = float(os.getenv("HOME_LAT"))
+MY_LONG = float(os.getenv("HOME_LONG"))
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
 
-data = response.json()
-t = Tk()
-t.withdraw()
-def iss_locator(data):
-    
+def get_iss_location() -> tuple:
+    """Returns a float numbers pair indicating International Space Station (ISS) current position."""
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
     iss_latitude = float(data["iss_position"]["latitude"])
-    iss_longuitude = float(data["iss_position"]["longitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    return iss_latitude, iss_longitude
 
-    #Your position is within +5 and -5 degrees of the ISS position
-    if (MY_LAT + 5) > iss_latitude > (MY_LAT-5) and (MY_LONG - 5)< iss_latitude < (MY_LONG + 5):
 
+def get_local_daylight(parameters: dict) -> tuple:
+    """Returns sunrise/sunset exact time from your given coordinates."""
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["result"]["sunset"].split("T")[1].split(":")[0])
+    return sunrise, sunset
+
+
+def iss_locator():
+    """Delivers an email when ISS is overhead the given position."""
+    while True:
+        iss_latitude, iss_longitude = get_iss_location()
+        # Your position is within +5 and -5 degrees of the ISS position
+        if (MY_LAT + 5) > iss_latitude > (MY_LAT - 5) and (MY_LONG - 5) < iss_longitude < (MY_LONG + 5):
             parameters = {
-                    "lat": MY_LAT,
-                    "lng": MY_LONG,
-                    "formatted":0,
+                "lat": MY_LAT,
+                "lng": MY_LONG,
+                "formatted": 0,
             }
-            response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-            response.raise_for_status()
-            data = response.json()
-            sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-            sunset = int(data["result"]["sunset"].split("T")[1].split(":")[0])
-
-            time_now = datetime.now()
-            my_hour = time_now.hour
-            if my_hour < sunrise and my_hour > sunset:
+            sunrise, sunset = get_local_daylight(parameters)
+            my_hour = datetime.now().hour
+            if sunrise > my_hour > sunset:
                 user = os.getenv("USER_MAIL")
                 pw = os.getenv("PSWD_MAIL")
-                serv = "smtp.gmail.com"
+                serv = os.getenv("SERV_MAIL")
+                port = int(os.getenv("PORT_MAIL"))
                 dest = "raulbvillamizar@gmail.com"
-                message = f"Subject: Hey, look up!!!\n\n The ISS is right now over your head"
-                with smtplib.SMTP(serv, 587) as email_conn:
+                message = f"Subject: Hey, look up!!!\n\n The ISS is right now over your head."
+                with smtplib.SMTP(serv, port) as email_conn:
                     email_conn.starttls()
                     email_conn.login(user, pw)
                     email_conn.sendmail(from_addr=user, to_addrs=dest, msg=message)
             else:
-                print("It's daylight. You won't be able to see anything")
-    else:
-        print("Nothing in the horizont, Captain")
-    t.after(60000, iss_locator, data)
+                print("It's daylight. You won't be able to see anything.")
+        time.sleep(60)
 
-if  __name__ == "__main__":
-    iss_locator(data)
 
-t.mainloop()
+if __name__ == "__main__":
+    iss_locator()
 
-#If the ISS is close to my current position
-#and it's currently dark
-#Then send me an email to tell me to look up
-#BONUS: run the code every 60 seconds
+# If the ISS is close to my current position,
+# and it's currently dark
+# Then email me telling to look up
+# BONUS: run the code every 60 seconds
