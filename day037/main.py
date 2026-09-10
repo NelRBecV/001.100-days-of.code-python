78b2d374ad02525baab0c59da7ceda0d7eac746a
Nelson Becerra <nelrbecv16@gmail.com>
2026-09-10 08:11:07 -0400

    Erase unnecessary comment lines

--- day037/main.py
+++ day037/main.py
@@ -8,14 +8,13 @@ load_dotenv()
 USERNAME = ""
 U_TOKEN = ""
 endpoint = "https://pixe.la/v1/users"
-PIXELA_PARAMS = {"username":USERNAME,#works with JSON
+PIXELA_PARAMS = {"username":USERNAME,
                  "token":U_TOKEN,
                  "agreeTermsOfService":"yes",
                  "notMinor":"yes",
 
 }
-# newcome = requests.post(url=endpoint, json=PIXELA_PARAMS)
-# print(newcome.text)
+
 #see pixela's API docs page
 endpoint_graph = f"{endpoint}/{USERNAME}/graphs"
 head = {"X-USER-TOKEN": U_TOKEN}
@@ -27,19 +26,16 @@ PIXELA_GRAPH_P = {
                   "type":"float",
                   "color":"sora"}#'blue' in japanesse
 
-# newcome = requests.post(url=endpoint_graph, json=PIXELA_GRAPH_P, headers=head)
-# print(newcome.text)
-# newcome.raise_for_status()
+
 my_graph = f"{endpoint_graph}/{my_graphs_id}"
 current_date = datetime(day=1, month=4, year=2025)
 date_activity = current_date.strftime('%Y%m%d')
 #strftime accepts any other date separator you wanna use (i.e. .strftime('%Y-%m-%'))
 quantity = "5"
-activity = {#'date':date_activity,
+activity = {
             'quantity':quantity
             }
 
-# my_graph_change = requests.post(url=my_graph,json=activity, headers=head)
-# my_graphs_change = requests.put(url=f"{my_graph}/{date_activity}", headers=head, json=activity)
+
 my_graph_change = requests.delete(url=f"{my_graph}/{date_activity}", headers=head)
 graph = webbrowser.open(f"{my_graph}.html")
